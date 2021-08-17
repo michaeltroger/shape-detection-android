81528c9ae98707f89544d5765b20ff90cab6bc51
Michael Troger <dev@michaeltroger.com>
2021-08-17 21:27:50 +0200

    Bump opencv

---
+++ opencv-3-4-15/native/jni/OpenCV-arm64-v8a.mk
@@ -0,0 +1,2 @@
+OPENCV_3RDPARTY_COMPONENTS:=quirc libprotobuf tbb cpufeatures ittnotify libjpeg-turbo libwebp libpng libtiff libjasper IlmImf tegra_hal
+OPENCV_EXTRA_COMPONENTS:=z dl m log
---
+++ opencv-3-4-15/native/jni/OpenCV-mips.mk
@@ -0,0 +1,2 @@
+OPENCV_3RDPARTY_COMPONENTS:=quirc libprotobuf tbb cpufeatures libjpeg-turbo libwebp libpng libtiff libjasper IlmImf
+OPENCV_EXTRA_COMPONENTS:=z dl m log
